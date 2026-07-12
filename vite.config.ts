e347191da433488fadd60cbde375728b55913d12
ElonInvestmentCo <managementelonmusk16@gmail.com>
2026-07-12 20:02:16 +0000

    FIX

--- vite.config.ts
+++ vite.config.ts
@@ -13,17 +13,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
     runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer()
-          ),
-          await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner()
-          ),
-        ]
-      : []),
   ],
   resolve: {
     alias: {
